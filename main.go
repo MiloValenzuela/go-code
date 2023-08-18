e0289a3b6e042d45e1732e9b5fb68b18021ed6d1
MiloValenzuela <40413618+MiloValenzuela@users.noreply.github.com>
2023-08-18 09:36:24 -0600

    for loop practice

--- main.go
+++ main.go
@@ -1,39 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-)
+import "fmt"
+
 
 func main() {
-	
-	// if t := time.Now(); t.Hour() < 12 {
-	// 	fmt.Println("Ma;ana!")
-	// } else if t.Hour() < 17 {
-	// 	fmt.Println("Tarde!")
-	// } else {
-	// 	fmt.Println("Noche!")
-	// }
+	saludo := hello("Milo")
+	fmt.Println(saludo)
+	sum, mul := calc(4, 5)
+	fmt.Println("La suma es: ", sum)
+	fmt.Println("La multipicacion es: ", mul)
+}
 
-	switch  t := time.Now(); {
-	case t.Hour() < 12:
-			fmt.Println("Ma;ana!")
-	case t.Hour() < 17:
-			fmt.Println("Tarde!")
-	default:
-		fmt.Println("Noche!")
-	}
+func hello(name string) string {
+	return "Hola, " + name
+}
 
-	os := runtime.GOOS
-	switch os {
-	case "windows":
-		fmt.Println("Go run -> Windows")
-	case "linux":
-		fmt.Println("Go run -> Linux")
-	case "darwin":
-		fmt.Println("Go run -> macOS")
-	default:
-		fmt.Println("Go run -> Otro OS")
-	}
+func calc(a, b int) (sum, mul int) {
+	sum = a + b
+	mul = a * b
+	return
 }
